f4e2dacbd2163a95da016aa869faba38cd066b24
yeeyunjie <yeeyunjie@gmail.com>
2024-01-11 15:58:20 +0800

    made changes to shared lib

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -4,6 +4,7 @@ def call(String repoUrl, String branch){
    def workingDir = "dummy"
    sh "mkdir ${workingDir}"
    sh "git clone ${repoUrl} ${workingDir}"
-   sh "git checkout ${branch}"
+   sh "pwd"
+//    sh "git checkout ${branch}"
    return workingDir
 }
