ef41da3e2ba7f33335292441814be70d0ac4c1f0
yeeyunjie <yeeyunjie@gmail.com>
2024-01-11 15:43:49 +0800

    changed wd

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(String repoUrl, String branch){
-   def workingDir = "${env.WORKSPACE}"
+   def workingDir = "dummy"
+   sh "mkdir ${workingDir}"
    sh "git clone ${repoUrl} ${workingDir}"
    sh "git checkout ${branch}"
    return workingDir
