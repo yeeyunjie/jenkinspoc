ff9a1b02e84dc2d7e1927a70f7f2fbb477ceb5f1
yeeyunjie <yee.yunjie@changiairport.com>
2024-01-12 11:10:54 +0800

    using map

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String name, String dayOfWeek ){
-   echo "hello ${name}"
-   echo "Today is ${dayOfWeek}"
+def call(Map config = [:]) {
+   echo "hello ${config.name}"
+   echo "Today is ${config.dayOfWeek}"
 }
